6e1980a74ad25e3ba3dab4cd51f13d8e333e7059
jackamrl <jacquesboyodi@yahoo.fr>
2022-12-20 18:25:26 +0000

    ini

--- application/config/database.php
+++ application/config/database.php
@@ -57,7 +57,7 @@ $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
-/*fffaafffff*/
+/*fffaafpkzzzfffffkjk777kff*/
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
