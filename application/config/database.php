8a70072f5e75aae45f467301cd65235edbf6228e
jackamrl <jacquesboyodi@yahoo.fr>
2022-12-20 18:30:00 +0000

    ini

--- application/config/database.php
+++ application/config/database.php
@@ -61,5 +61,17 @@ $db['default']['stricton'] = FALSE;
 /*f*/
 /*f*/
 
+/*f*/
+/*f*/
+/*f*/
+/*f*/
+/*f*/
+/*f*/
+/*f*/
+/*f*/
+/*f*/
+/*f*/
+/*f*/
+/*f*/
 /* End of file database.php */
 /* Location: ./application/config/database.php */
